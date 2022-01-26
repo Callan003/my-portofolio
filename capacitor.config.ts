2db49cc14a054f57981745a1544fcd9773ade544
Stroescu Marian-Calin <cali_stroescu@yahoo.com>
2022-01-26 13:13:46 +0200

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.mcstroescu.portofolio',
   appName: 'my-portofolio',
   webDir: 'www',
   bundledWebRuntime: false
